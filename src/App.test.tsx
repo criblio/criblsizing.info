ee1ebe13e53c26303587dfade63214f1c235ba25
Brendan Dalpe <bdalpe@gmail.com>
2021-09-20 17:29:25 -0500

    Add basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders coming soon text', () => {
+test('Ensure calc box is shown with defaults', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Coming soon.../i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Calculations/i)).toBeInTheDocument();
+  expect(screen.getByText(/2 workers/i)).toBeInTheDocument();
+});
+
+test('Ensure it updates correctly', () => {
+  render(<App />);
+
+  const inbound = screen.getByTestId("inbound");
+  fireEvent.change(inbound, {target: {value: 10}})
+
+  const input = screen.getByTestId("cpu-speed");
+  userEvent.type(input,".6");
+
+  expect(screen.getByTestId("cpu-speed")).toHaveValue(3.6);
+  expect(screen.getByText(/43 processes/i)).toBeInTheDocument();
+  expect(screen.getByText(/21 workers/i)).toBeInTheDocument();
 });

22c2771fcdb229d9202f50c5e294c13762f618d0
Brendan Dalpe <bdalpe@gmail.com>
2021-09-20 19:36:17 -0500

    Update links

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,8 +7,8 @@ const Header = () => {
     return (
         <Navbar bg={"dark"} variant={"dark"}>
             <Container fluid={true}>
-                <Navbar.Brand href={"#"} className={"p-0"}><img src={logo} alt="cribl" height={30}/></Navbar.Brand>
-                <Nav.Link className={"ms-auto p-0"} href={"https://github.com/bdalpe/criblsizing.info"}><img src={gh} alt={"Github"} height={30} /></Nav.Link>
+                <Navbar.Brand href={"https://cribl.io"} target={"_blank"} className={"p-0"}><img src={logo} alt="cribl" height={30}/></Navbar.Brand>
+                <Nav.Link className={"ms-auto p-0"} href={"https://github.com/bdalpe/criblsizing.info"} target={"_blank"}><img src={gh} alt={"Github"} height={30} /></Nav.Link>
             </Container>
         </Navbar>
     );
